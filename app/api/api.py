79fe1b3441ebb11970a00154d5cbccf2c6685074
Ollie <o.culver1@newcastle.ac.uk>
2025-08-04 21:46:11 +0100

    Swapped the code in main.py with app/api.py. Then added my new routers into api.py file. Also added an import statement in the __init__.py in the api directory, in order that the api with all its routers can be accessed in main.py

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
-from app.api import goals
 
-router = APIRouter()
+from app.api.goals import router as goals_router
+from app.api.todos import router as todos_router
+from app.api.workouts import router as workouts_router
 
-# Include each feature's router
-router.include_router(goals.router)
+api_router = APIRouter()
+
+# Include each router with a base path
+api_router.include_router(goals_router, prefix="/goals", tags=["Goals"])
+api_router.include_router(todos_router, prefix="/todos", tags=["Todos"])
+api_router.include_router(workouts_router, prefix="/workouts", tags=["Workouts"])
--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from app.api import api
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from app.api import api_router
 
 app = FastAPI()
-app.include_router(api.router)
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
+
+# Mount the full API
+app.include_router(api_router)
